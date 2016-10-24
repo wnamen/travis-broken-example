5a099ca355321d6e072fef74d8f999bab531c1ea
wnamen <williamjosephnamen@gmail.com>
2016-10-24 11:26:52 -0700

    updated file

--- Test.php
+++ Test.php
@@ -3,8 +3,12 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
+		$this->assertEquals(1+1,2);
   	}
+
+	// public function testOnePlusOne() {
+	// 	$this->assertEquals(1+4,4);
+	// 	}
 }
 
 ?>
